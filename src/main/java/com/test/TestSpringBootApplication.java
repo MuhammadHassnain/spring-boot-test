a4fdbe53be2122c299190eefb62e00cf2083c3a5
Muhammad Hassnain <muhammadhassnain776@gmail.com>
2021-12-30 22:12:54 +0500

    Add Rest End point.

--- src/main/java/com/test/TestSpringBootApplication.java
+++ src/main/java/com/test/TestSpringBootApplication.java
@@ -2,11 +2,19 @@ package com.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TestSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestSpringBootApplication.class, args);
+	}
+	
+	@GetMapping("/test")
+	public String test() {
+		return "Hello from spring";
 	}
 }
